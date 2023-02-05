8d802ecb78b9761eca9a19ddcf4414d3044fcecb
Pamella Silva <Pamellasilva296@gmail.com>
2023-02-05 02:16:25 -0300

    primeiras aulas do curso de java básico da Loiane

---
+++ Aula05/HelloWorld.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+	public static void main (String[] args){
+		System.out.println("Hello World!");
+	}
+}
+
+// 1- declaramos uma classe. é uma linguagem orientada a obejeto, então tudo no java é um objeto. objeto é uma instÂncia de uma classe.
+// 2- HelloWolrd é o nome de nossa classe
+// 3- dentro da chave da nossa classe é um método. Os objetos/classes possuem métodos que são funçoes que uma classe pode exercer.
+// 4- public static vois main passando um argumento: um array (coleção) de strings como argumentos para o método (não muda).
+// 5- public static são modificadores de acesso do método
+// 6- void é o tipo de retorno do método
+// 7- main é o nome do método
+// 8- java é casesensitive (diferencia maiúsculo de minúsculo)
+// 9- entre parenteses são os parâmetros/argumentos do método. o método main pode receber argumentos
+// 10- system.ou.println é o comando de saída de dados(imprime na nossa tela)
+// 11- cada parte do código é chamado de blocos e eles precisam estar entre chaves ({})
+ 
+ 
+  
---
+++ Aula05/OlaMundo.java
@@ -0,0 +1,8 @@
+// --> lição de casa: 1- criar um arquivo chamado OlaMundo.java, 2- Implementar um programa Java que escreva na tela "Olá Mundo!", 3- Compilar e Executar o programa.
+
+class OlaMundo {
+	public static void main (String[] args){
+		System.out.println("Ola Mundo!");
+	}
+
+}
